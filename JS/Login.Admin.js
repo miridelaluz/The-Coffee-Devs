0fcadb62aa60591aef1986c63386669ad16f664c
Valeria Mojica <mojicacastanedavaleria@gmail.com>
2022-07-29 16:39:14 -0500

    Update: actualizacion de body

--- JS/Login.Admin.js
+++
@@ -1,7 +0,0 @@
-function previewImage(nb) {        
-    var reader = new FileReader();         
-    reader.readAsDataURL(document.getElementById('uploadImage'+nb).files[0]);         
-    reader.onload = function (e) {             
-        document.getElementById('uploadPreview'+nb).src = e.target.result;         
-    };     
-}
