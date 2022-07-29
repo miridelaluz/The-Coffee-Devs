0e6d7ba3f5c0fe33e067d155b567ab0aa472cf2e
Johana Arredondo <johana.arredondo3402@alumnos.udg.mx>
2022-07-29 12:02:19 -0500

    borrar

--- logi.js
+++
@@ -1,9 +0,0 @@
-function visualizar(nb) {        
-    var leer = new FileReader();         
-    leer.readAsDataURL(document.getElementById('imagenCargada'+nb).files[0]);         
-    leer.onload = function (e) {             
-        document.getElementById('visualizar'+nb).src = e.target.result;         
-    };     
-}
-
-
