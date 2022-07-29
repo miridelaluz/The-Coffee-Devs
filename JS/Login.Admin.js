d4fb43b5d7bd322e57b0aef3bea76d612bd95f78
Johana Arredondo <johana.arredondo3402@alumnos.udg.mx>
2022-07-29 15:21:48 -0500

    actualizacion

--- JS/Login.Admin.js
+++ JS/Login.Admin.js
@@ -1,7 +1,16 @@
-function previewImage(nb) {        
+/* function previewImage(nb) {        
     var reader = new FileReader();         
     reader.readAsDataURL(document.getElementById('uploadImage'+nb).files[0]);         
     reader.onload = function (e) {             
         document.getElementById('uploadPreview'+nb).src = e.target.result;         
+    };     
+} */
+
+
+function previ(nb) {        
+    var leer = new FileReader();         
+    leer.readAsDataURL(document.getElementById('cargar'+nb).files[0]);         
+    leer.onload = function (e) {             
+        document.getElementById('previ'+nb).src = e.target.result;         
     };     
 }
