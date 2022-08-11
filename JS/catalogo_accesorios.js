08a9fa2ff503971240bfa8e1b772861dee143934
Sebastian Delgado <sebastian.d.alex@gmail.com>
2022-08-10 21:03:42 -0500

    add-json joyas

---
+++ JS/catalogo_accesorios.js
@@ -0,0 +1,79 @@
+//Links de las categorias
+const joyeria = "/data/catalogo_camisas.json";
+const talegas = "/data/catalogo_tunicas.json";
+const zurrones = "/data/catalogo_calzones.json";
+const sporrans = "/data/catalogo_calzas.json";
+const cinturones = "/data/catalogo_winingas.json"
+const bolsos = "/data/catalogo_camisas.json";
+const sombreros = "/data/catalogo_tunicas.json";
+const bonetes = "/data/catalogo_calzones.json";
+const capirotes = "/data/catalogo_calzas.json";
+const cofias = "/data/catalogo_winingas.json"
+const llave = "datosProductos";
+
+/* function mostrarDatos(e) {
+    e.preventDefault
+    console.log(e.target.parentElement.classList.contains('categorias'));}
+ */
+ function mostrarDatos(categoria) {
+    fetch(categoria)
+        .then ( (responseJSON) => { return responseJSON.json()})
+        .then(prendas => {
+        let contenedor = document.getElementById("contenedor_cards");
+        contenedor.innerHTML = ``;   
+        for (let prenda of prendas.data) {
+           let div = `
+           <div class="card_producto">
+                <div>
+                    <img class="imagen_producto" src="${prenda.imagen}" alt="">
+                </div>
+                <div class="contenido">
+                    <h3 class="titulo_producto">${prenda.nombre_producto}</h3>
+                    <h5 class="descripcion">${prenda.descripcion}</h5>
+                    <p class="contenido_producto">
+                    <form>
+                    <label><strong>Talla: </strong></label> 
+                    <select name="tallas">
+                        <option>${prenda.tallas[0]}</option>
+                        <option>${prenda.tallas[1]}</option>
+                        <option>${prenda.tallas[2]}</option>
+                        <option>${prenda.tallas[3]}</option>
+                    </select> <br>
+                    <label><strong>Color: </strong></label>
+                    <select name="color">
+                        <option>${prenda.color[0]}</option>
+                        <option>${prenda.color[1]}</option>
+                        <option>${prenda.color[2]}</option>
+                    </select> <br>
+                    <strong>Material: </strong> ${prenda.material[0]} Lino<br>
+                    <strong>Precio: </strong> $ ${prenda.precio} MXN<br>
+                </form>
+                    </p>
+                    <a href="" class="enlaces">Agregar al carrito</a>
+                    <a>
+                        <button class="icono"><img src="/assets/iconos/5172567_heart_like_love_icon.png" alt="" height="45px"> </button>
+                    </a>
+                </div>
+            </div>`;
+            contenedor.insertAdjacentHTML("afterbegin",div);
+        }
+        });
+
+        console.log('hey');
+}
+
+//Las variables para los enlaces 
+const aCamisas = document.getElementById("camisas");
+const aTunicas = document.getElementById("tunicaIn");
+const aCalzones = document.getElementById("calzones");
+const aCalzas = document.getElementById("calzas");
+const aWiningas = document.getElementById("winingas");
+
+aCamisas.addEventListener('click', () => {mostrarDatos(camisas)});//mostrarDatos(camisas));
+aTunicas.addEventListener('click', () => {mostrarDatos(tunicas)});
+aCalzones.addEventListener('click', () => {mostrarDatos(calzones)});
+aCalzas.addEventListener('click', () => {mostrarDatos(calzas)});
+aWiningas.addEventListener('click', () => {mostrarDatos(winingas)});
+
+
+//document.getElementById('camisas').addEventListener('click',console.log('hey'));
