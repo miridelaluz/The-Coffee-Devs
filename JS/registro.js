3caeddf83273ef31492ad7000f4ebb03ee24b9f1
Alexs3221 <alexsm92.11.25@gmail.com>
2022-08-19 05:39:24 -0500

    modify: registro

--- JS/registro.js
+++ JS/registro.js
@@ -1,9 +1,7 @@
 //Trae los inputs del formulario
 let nameContact = document.getElementById("nameContact");
-let ApellidoPContact = document.getElementById("ApellidoPContact");
-let ApellidoMContact = document.getElementById("ApellidoMContact");
 let fechaNac = document.getElementById("birthDate");
-let country = document.getElementById("country")
+let numberContact = document.getElementById("numberContact")
 let mailContact = document.getElementById("mailContact")
 let passwordContact = document.getElementById("passwordContact")
 let confirPassContact = document.getElementById("confirPassContact")
@@ -24,48 +22,8 @@ function validarNombre() {
     }
 } //Validación nombre
 
-function validarApellidoP() {
-    if (ApellidoPContact.value.length < 3) {
-        ApellidoPContact.style.border = "red thin solid";
-        document.getElementById("alertnombreP").innerHTML = "Dato inválido, tu apellido debe contener más de 3 caracteres";
-        document.getElementById("alertnombreP").style = "display: block; margin-bottom: -10px;";
-        return false;
-    } else {
-        ApellidoPContact.style.border = "green thin solid";
-        document.getElementById("alertnombreP").style.display = "none";
-        return true;
-    }
-} // Validación apellido paterno
 
-function validarApellidoM() {
-    if (ApellidoMContact.value.length < 3) {
-        ApellidoMContact.style.border = "red thin solid";
-        document.getElementById("alertnombreM").innerHTML = "Dato inválido, tu apellido debe contener más de 3 caracteres";
-        document.getElementById("alertnombreM").style = "display: block; margin-bottom: -10px;";
-        return false;
-    } else {
-        ApellidoMContact.style.border = "green thin solid";
-        document.getElementById("alertnombreM").style.display = "none";
-        return true;
-    }
-} // Validación apellido materno
-
-let validacionBD = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-
-function validarfechaNac() {
-    // console.log(birthDate.value);
-    // console.log(validacionBD);
-    if (!validacionBD.test(fechaNac.value)) {
-        fechaNac.style.border = "red thin solid";
-        document.getElementById("alertBD").innerHTML = "Debes ingresar una fecha de nacimiento válida";
-        document.getElementById("alertBD").style = "display: block; margin-bottom: -10px;";
-        return false;
-    } else {
-        birthDate.style.border = "green thin solid";
-        document.getElementById("alertBD").style.display = "none";
-        return true;
-    }
-} // Validación Fecha de Nacimiento
+ // Validación Fecha de Nacimiento
 
 function validarContraseña() {
     if (passwordContact.value.length < 5) {
@@ -94,18 +52,20 @@ function validarConfirContraseña() {
 } //Validación confirmación de contraseña
 
 
-function validarPais() {
-    if (!validacionPais.test(country.value)) {
-        country.style.border = "red thin solid";
-        document.getElementById("alertPais").innerHTML = "Debes seleccionar un país";
-        document.getElementById("alertPais").style = "display: block; margin-bottom: -10px;";
+let validacionCel = /^[0-9]{10}$/;
+
+function validarNumero() {
+    if (!validacionCel.test(numberContact.value)) {
+        numberContact.style.border = "red thin solid";
+        document.getElementById("alertnum").innerHTML = "Tu número debe tener 10 dígitos";
+        document.getElementById("alertnum").style = "display: block; margin-bottom: -10px;";
         return false;
     } else {
-        country.style.border = "green thin solid";
-        document.getElementById("alertPais").style.display = "none";
+        numberContact.style.border = "green thin solid";
+        document.getElementById("alertnum").style.display = "none";
         return true;
     }
-} // Validación de País
+} // Validación de celular
 
 let validacionEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
@@ -154,22 +114,9 @@ nameContact.addEventListener("blur", (e) => {
         validarNombre();
     }) //Nombre
 
-ApellidoPContact.addEventListener("blur", (e) => {
-        e.target.value = e.target.value.trim();
-        validarApellidoP();
-    }) //Apellido P
 
-ApellidoMContact.addEventListener("blur", (e) => {
-        e.target.value = e.target.value.trim();
-        validarApellidoM();
-    }) //Apellido M
 
-fechaNac.addEventListener("blur", (e) => {
-        e.target.value = e.target.value.trim();
-        validarfechaNac();
-    }) //Fecha Nacimiento
-
-country.addEventListener("blur", (e) => {
+numberContact.addEventListener("blur", (e) => {
         e.target.value = e.target.value.trim();
         validarNumero();
     }) //Número
@@ -199,17 +146,14 @@ enviar.addEventListener("click", (event) => {
 
     // Se hacen validaciones
     validarNombre();
-    validarApellidoP();
-    validarApellidoM();
-    validarfechaNac();
     validarContraseña();
     validarConfirContraseña();
-    validarPais();
+    validarNumero();
     validarEmail();
     validarPoliticas();
     123
     //Si falla alguna validacion, se muestra alerta de error 
-    if ((!validarNombre()) || (!validarContraseña()) || (!validarApellidoM()) || (!validarNumero()) || (!validarConfirContraseña()) || (!validarApellidoP()) || (!validarEmail()) || (!validarPoliticas()) || (!validarfechaNac())) {
+    if ((!validarNombre()) || (!validarContraseña())  || (!validarNumero()) || (!validarConfirContraseña()) || (!validarEmail()) || (!validarPoliticas()) ) {
         Swal.fire({
             icon: 'error',
             title: 'Error',
@@ -221,11 +165,9 @@ enviar.addEventListener("click", (event) => {
 
     let newUsuario = {
         user_name: document.querySelector(`#nameContact`).value,
-        user_lastNF: document.querySelector(`#ApellidoPContact`).value,
-        user_lastNM: document.querySelector(`#ApellidoMContact`).value,
         user_date: document.querySelector(`#birthDate`).value,
         user_pass: document.querySelector(`#passwordContact`).value,
-        user_country: document.querySelector(`#country`).value,
+        user_phone: document.querySelector(`#numberContact`).value,
         user_email: document.querySelector(`#mailContact`).value,
         user_type_id: 1
     };
@@ -266,11 +208,11 @@ enviar.addEventListener("click", (event) => {
 
             }
         });
-    
+
 });
 
 window.addEventListener("load", function() {
     if (localStorage.getItem("Usuario") != null) {
         usuarios = JSON.parse(localStorage.getItem("Usuario"));
     } // if
-});
+}); 
